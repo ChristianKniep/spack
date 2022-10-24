cc44ecb3519e1b7e00c4c4235697fa8b0c8bddb2
Christian Kniep <christian@qnib.org>
2022-10-24 13:07:41 +0200

    update bbmap to 38.93

--- var/spack/repos/builtin/packages/bbmap/package.py
+++ var/spack/repos/builtin/packages/bbmap/package.py
@@ -12,6 +12,7 @@ class Bbmap(Package, SourceforgePackage):
     homepage = "https://sourceforge.net/projects/bbmap/"
     sourceforge_mirror_path = "bbmap/BBMap_38.63.tar.gz"
 
+    version("38.93", sha256="e2700cc6bfa6ff0868bb2887850f1e514bc6e44cd71ca933e23ae5ce3c630605")
     version("38.63", sha256="089064104526c8d696164aefa067f935b888bc71ef95527c72a98c17ee90a01f")
     version("37.36", sha256="befe76d7d6f3d0f0cd79b8a01004a2283bdc0b5ab21b0743e9dbde7c7d79e8a9")
 
